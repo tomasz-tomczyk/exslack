c3c3b82358061fa575f5ee90f0222a611ff96c86
Tomasz Tomczyk <me@tomasztomczyk.com>
2022-07-10 19:43:40 +0100

    Fix tests

--- test/utils/raw_body_test.exs
+++ test/utils/raw_body_test.exs
@@ -2,23 +2,17 @@ defmodule ExSlack.Utils.CacheBodyReaderTest do
   use ExUnit.Case, async: true
   use Plug.Test
 
-  # These are not really what's intended, pending feedback on
-  # https://github.com/elixir-plug/plug/pull/1110
   test "saves raw_body value" do
-    assert {:ok, "", conn} =
-             conn(:get, "/test", %{
-               "hello" => "world"
-             })
+    assert {:ok, _body, conn} =
+             conn(:get, "/test", "{\"hello\":\"world\"")
              |> ExSlack.Utils.CacheBodyReader.read_body()
 
-    assert conn.assigns.raw_body == [""]
+    assert conn.assigns.raw_body == ["{\"hello\":\"world\""]
 
-    assert {:ok, "--plug_conn_test--", conn} =
-             conn(:post, "/test", %{
-               "hello" => "world"
-             })
+    assert {:ok, _body, conn} =
+             conn(:post, "/test", "{\"hello\":\"world\"")
              |> ExSlack.Utils.CacheBodyReader.read_body()
 
-    assert conn.assigns.raw_body == ["--plug_conn_test--"]
+    assert conn.assigns.raw_body == ["{\"hello\":\"world\""]
   end
 end
